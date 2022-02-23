30f44551643a2934e048785c35844714c9815f01
Dalianis Ioannis <39140479+DalianisIoannis@users.noreply.github.com>
2022-02-24 01:44:07 +0200

    Add files via upload

---
+++ heapfile/examples/hp_mainNEW.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "bf.h"
+#include "heap_file.h"
+
+#define RECORDS_NUM 1700
+
+const char* names[] = {
+  "Yannis",
+  "Christofos",
+  "Sofia",
+  "Marianna",
+  "Vagelis",
+  "Maria",
+  "Iosif",
+  "Dionisis",
+  "Konstantina",
+  "Theofilos",
+  "Giorgos",
+  "Dimitris"
+};
+
+const char* surnames[] = {
+  "Ioannidis",
+  "Svingos",
+  "Karvounari",
+  "Rezkalla",
+  "Nikolopoulos",
+  "Berreta",
+  "Koronis",
+  "Gaitanis",
+  "Oikonomou",
+  "Mailis",
+  "Michas",
+  "Halatsis"
+};
+
+const char* cities[] = {
+  "Athens",
+  "San Francisco",
+  "Los Angeles",
+  "Amsterdam",
+  "London",
+  "New York",
+  "Tokyo",
+  "Hong Kong",
+  "Munich",
+  "Miami"
+};
+
+#define CALL_OR_DIE(call)     \
+  {                           \
+    HP_ErrorCode code = call; \
+    if (code != HP_OK) {      \
+      printf("Error!!!\n");      \
+      exit(code);             \
+    }                         \
+  }
+
+#define RUN_AND_TIME(call)                                      \
+  {                                                             \
+    clock_t start = clock();                                    \
+    call;                                                       \
+    clock_t end = clock();                                      \
+    printf("CPU Time: %lf\n", (end - start) / (double)CLOCKS_PER_SEC);  \
+  }
+
+void TestFileScan(int fileDesc) {
+  Record record;
+  // printf("id,name,surname,city\n");
+  for (int j = 1; j <= 500; ++j) {
+    for (int id = RECORDS_NUM; id != 0; --id) {
+      CALL_OR_DIE(HP_GetEntry(fileDesc, id, &record));
+      // printf("%d,\"%s\",\"%s\",\"%s\"\n",record.id, record.name, record.surname, record.city);
+    }
+  }
+}
+
+int main() {
+  
+  BF_Init(LRU);
+  //BF_Init(MRU);
+
+  CALL_OR_DIE(HP_Init());
+
+  int fd;
+  CALL_OR_DIE(HP_CreateFile("data.db"));
+  CALL_OR_DIE(HP_OpenFile("data.db", &fd));
+
+  Record record;
+  srand(12569874);
+  int r;
+  printf("Insert Entries\n");
+  for (int id = 0; id < RECORDS_NUM; ++id) {
+    record.id = id;
+    r = rand() % 12;
+    memcpy(record.name, names[r], strlen(names[r]) + 1);
+    r = rand() % 12;
+    memcpy(record.surname, surnames[r], strlen(surnames[r]) + 1);
+    r = rand() % 10;
+    memcpy(record.city, cities[r], strlen(cities[r]) + 1);
+
+    CALL_OR_DIE(HP_InsertEntry(fd, record));
+  }
+
+  printf("RUN TestFileScan\n");
+  RUN_AND_TIME(TestFileScan(fd));
+
+  // printf("RUN PrintAllEntries\n");
+  // CALL_OR_DIE(HP_PrintAllEntries(fd, "city", (void *)NULL));
+
+  // printf("RUN PrintAllEntries with id 10\n");
+  // CALL_OR_DIE(HP_PrintAllEntries(fd, "id", (void *)10));
+
+  // printf("RUN PrintAllEntries with name Dionisis\n");
+  // CALL_OR_DIE(HP_PrintAllEntries(fd, "name", "Dionisis"));
+
+  // printf("RUN PrintAllEntries with surname Nikolopoulos\n");
+  // CALL_OR_DIE(HP_PrintAllEntries(fd, "surname", "Nikolopoulos"));
+
+  // printf("RUN PrintAllEntries with city Amsterdam\n");
+  // CALL_OR_DIE(HP_PrintAllEntries(fd, "city", "Amsterdam"));
+
+  // for(int j=1; j<10; j++){
+  //   printf("Get Entry with rowid %d\n",j);
+  //   CALL_OR_DIE(HP_GetEntry(fd, j, &record));
+  //   printf("%d,\"%s\",\"%s\",\"%s\"\n",record.id, record.name, record.surname, record.city);
+  // }
+
+  CALL_OR_DIE(HP_CloseFile(fd));
+  BF_Close();
+  return 0;
+}
